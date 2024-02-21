3e4e16d1d5dbdd19f21a2bbfb3a3ffc26c198acb
Johan Santana <johancarlosst@gmail.com>
2024-02-20 21:33:26 -0400

    fix: remove url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const URL =
-  'https://docs.google.com/forms/d/e/1FAIpQLScakAhW2zEtQLMFD7sIHVbJ00UKHUhyr08dIhiIqqmAIYV8zQ/viewform?usp=sf_link'
+export const URL = ''
 export const USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 export const PARTICIPANTS_COUNT = 20 // Change numbers
