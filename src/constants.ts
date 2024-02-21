b360cf78ed68359019d422f4448fd366cbc55846
Johan Santana <johancarlosst@gmail.com>
2024-02-20 21:34:31 -0400

    chore: reorder

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-export const URL = ''
 export const USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-export const PARTICIPANTS_COUNT = 20 // Change numbers
+
+// Modify
+export const URL = '' // Form Url
+export const PARTICIPANTS_COUNT = 20 // Number of Answers
 
 export const QUESTIONS_MAP = [
   // { value: 0, probability: 0 },
